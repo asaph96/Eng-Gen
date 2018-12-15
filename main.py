9c96c4daea5cf73453ae9492b261946757e33aff
asaph96 <asaph@ufu.br>
2018-12-15 16:45:55 -0200

    adicionado degeneração de ácido nucleico

--- main.py
+++ main.py
@@ -38,18 +38,35 @@ class Proteina(MaterialGenetico):
 
 class AcidoNucleico(MaterialGenetico):
 
-    def __init__(self, sequencia):
+    def __init__(self, sequencia, degenerado=False):
         super().__init__(sequencia)
         self.seq = sequencia
+        self.deg = degenerado
         print("Ácido nucleico criado com sucesso")
 
+    def _is_valid(self):
+        if not self.deg:
+            for elemento in self.seq.upper():
+                if elemento not in self._lexico[0]:
+                    raise ValueError('Sequência inválida')
+                else:
+                    pass
+            return True
+        else:
+            for elemento in self.seq.upper():
+                if elemento not in self._lexico[0] and elemento not in self._lexico[1]:
+                    raise ValueError('Sequência inválida')
+                else:
+                    pass
+        return True
 
 class DNA(AcidoNucleico):
     _lexico = [('A', 'T', 'G', 'C'), ('W', 'S', 'M', 'K',
                                       'R', 'Y', 'B', 'D', 'H', 'V', 'N'), ]
 
-    def __init__(self, sequencia):
-        super().__init__(sequencia)
+    def __init__(self, sequencia, degenerado=False):
+        super().__init__(sequencia, degenerado)
+        self.deg = degenerado
         self.seq = sequencia
 
 
@@ -57,6 +74,7 @@ class RNA(AcidoNucleico):
     _lexico = [('A', 'U', 'G', 'C', 'I'), ('W', 'S', 'M', 'K',
                                            'R', 'Y', 'B', 'D', 'H', 'V', 'N'), ]
 
-    def __init__(self, sequencia):
-        super().__init__(sequencia)
+    def __init__(self, sequencia, degenerado=False):
+        super().__init__(sequencia, degenerado)
+        self.deg = degenerado
         self.seq = sequencia
