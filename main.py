94a5e5399e6822abd45633978370e2ce490b59c9
asaph96 <asaph@ufu.br>
2018-12-15 18:12:08 -0200

    Reverso complementar implementado

--- main.py
+++ main.py
@@ -60,22 +60,69 @@ class AcidoNucleico(MaterialGenetico):
                     pass
             return True
 
+    def _reverso_complementar(self):
+        rev_comp = ''
+        index = 1
+        try:
+            for nucleotideo in self.seq:
+                if nucleotideo == self._lexico[0][1]:
+                    rev_comp += self._lexico[0][0]
+                elif nucleotideo == self._lexico[0][0]:
+                    rev_comp += self._lexico[0][1]
+                elif nucleotideo == self._lexico[0][3]:
+                    rev_comp += self._lexico[0][2]
+                elif nucleotideo == self._lexico[0][2]:
+                    rev_comp += self._lexico[0][3]
+                elif nucleotideo == self._lexico[0][4]:
+                    rev_comp += '*'
+                elif nucleotideo == self._lexico[1][1]:
+                    rev_comp += self._lexico[1][0]
+                elif nucleotideo == self._lexico[1][0]:
+                    rev_comp += self._lexico[1][1]
+                elif nucleotideo == self._lexico[1][3]:
+                    rev_comp += self._lexico[1][2]
+                elif nucleotideo == self._lexico[1][2]:
+                    rev_comp += self._lexico[1][3]
+                elif nucleotideo == self._lexico[1][5]:
+                    rev_comp += self._lexico[1][4]
+                elif nucleotideo == self._lexico[1][4]:
+                    rev_comp += self._lexico[1][5]
+                elif nucleotideo == self._lexico[1][7]:
+                    rev_comp += self._lexico[1][6]
+                elif nucleotideo == self._lexico[1][6]:
+                    rev_comp += self._lexico[1][7]
+                elif nucleotideo == self._lexico[1][9]:
+                    rev_comp += self._lexico[1][8]
+                elif nucleotideo == self._lexico[1][8]:
+                    rev_comp += self._lexico[1][9]
+                else:
+                    rev_comp += self._lexico[1][10]
+                index += 1
+        except IndexError:
+            print('O nucleotídeo %s da posição %i não tem par'%(nucleotideo, index,))
+        finally:
+            return rev_comp
+
 
 class DNA(AcidoNucleico):
     _lexico = [('A', 'T', 'G', 'C'), ('W', 'S', 'M', 'K',
-                                      'R', 'Y', 'B', 'D', 'H', 'V', 'N'), ]
+                                      'R', 'Y', 'B', 'V', 'H', 'D', 'N'), ]
 
     def __init__(self, sequencia, degenerado=False):
         self.deg = degenerado
-        super().__init__(sequencia, self.deg)        
+        super().__init__(sequencia, self.deg)
         self.seq = sequencia
+        print("DNA criado com sucesso")
+        self.reverso_complementar = self._reverso_complementar()
 
 
 class RNA(AcidoNucleico):
     _lexico = [('A', 'U', 'G', 'C', 'I'), ('W', 'S', 'M', 'K',
-                                           'R', 'Y', 'B', 'D', 'H', 'V', 'N'), ]
+                                           'R', 'Y', 'B', 'V', 'H', 'D', 'N'), ]
 
     def __init__(self, sequencia, degenerado=False):
         self.deg = degenerado
-        super().__init__(sequencia, self.deg)        
+        super().__init__(sequencia, self.deg)
         self.seq = sequencia
+        print("RNA criado com sucesso")
+        self.reverso_complementar = self._reverso_complementar()
